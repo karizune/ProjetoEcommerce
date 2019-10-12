24917c282f339230b47fe664348124c33ea40e10
Alefe Eliel <candidoalefe@outlook.com>
2019-10-11 21:58:01 -0300

    Adicao da classe CarrinhoProduto

---
+++ Desenvolvimento/SRC/ProjetoEcommerce/ProjetoEcommerce.Dominio/Entidades/Carrinho/CarrinhoProduto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoEcommerce.Dominio.Entidades.Carrinho
+{
+    class CarrinhoProduto
+    {
+
+
+        //LOG
+        public int Status{ get; set; }
+        public string Usuario { get; set; }
+        public DateTime CriadoEm { get; set; }
+        public DateTime AtualizadoEm { get; set; }
+
+        //
+        public int CarrinhoID { get; set; }
+        public int Quantidade { get; set; }
+        public decimal PrecoUnitario { get; set; }
+
+    }
+}
