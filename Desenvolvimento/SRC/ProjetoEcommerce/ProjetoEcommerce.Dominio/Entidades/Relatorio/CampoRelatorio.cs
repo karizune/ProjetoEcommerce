618ef8c62dee0d77bf912c4f51f0c71f2bb20209
gabriel <gabriel.henrique@outlook.com>
2019-10-11 21:53:08 -0300

    Criação Classe Campo Relatorio

--- Desenvolvimento/SRC/ProjetoEcommerce/ProjetoEcommerce.Dominio/Entidades/Relatorio/CampoRelatorio.cs
+++ Desenvolvimento/SRC/ProjetoEcommerce/ProjetoEcommerce.Dominio/Entidades/Relatorio/CampoRelatorio.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProjetoEcommerce.Dominio.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace ProjetoEcommerce.Dominio.Entidades.Relatorio
 {
-    public class CampoRelatorio
+    public class CampoRelatorio: IEntidadeBase
     {
         public int RelatorioID { get; set; }
         public int CampoID { get; set; }
@@ -14,6 +15,5 @@ namespace ProjetoEcommerce.Dominio.Entidades.Relatorio
         public int Status { get; set; }
         public DateTime CriadoEm { get; set; }
         public DateTime AtualizadoEm { get; set; }
-
     }
 }
