87657cdc6243b338f8f20d90d08a1044ebcccda4
Walber Fellipe <Walberfellipe97@outlook.com>
2019-10-11 21:50:06 -0300

    Criação da classe Registro

---
+++ Desenvolvimento/SRC/ProjetoEcommerce/ProjetoEcommerce.Dominio/Entidades/Relatorio/Registro.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoEcommerce.Dominio.Entidades.Relatorio
+{
+    public class Registro
+    {
+        public int RelatorioID { get; set; }
+        public int UsuarioID { get; set; }
+
+
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+
+
+        //LOG
+        public int Status { get; set; }
+        public string Usuario { get; set; }
+        public DateTime CriadoEm { get; set; }
+        public DateTime AtualizadoEm { get; set; }
+
+
+    }
+}
