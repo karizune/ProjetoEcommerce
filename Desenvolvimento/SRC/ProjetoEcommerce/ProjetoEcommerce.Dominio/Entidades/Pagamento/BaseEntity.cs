9d1acd9bd7e5182d3c8f7632e09fe1f00fbebd3e
luan galan <luanchrystian2@hotmail.com>
2019-10-11 21:39:19 -0300

    change in BaseEntity, altering order the proprietities.

--- Desenvolvimento/SRC/ProjetoEcommerce/ProjetoEcommerce.Dominio/Entidades/Pagamento/BaseEntity.cs
+++ Desenvolvimento/SRC/ProjetoEcommerce/ProjetoEcommerce.Dominio/Entidades/Pagamento/BaseEntity.cs
@@ -4,8 +4,8 @@ namespace ProjetoEcommerce.Dominio.Entidades.Pagamento
 {
     public class BaseEntity
     {
-        public DateTime CriadoEm { get; set; }
         public DateTime AtualizadoEm { get; set; }
+        public DateTime CriadoEm { get; set; }
         public int Status { get; set; }
         public string Usuario { get; set; }
     }
