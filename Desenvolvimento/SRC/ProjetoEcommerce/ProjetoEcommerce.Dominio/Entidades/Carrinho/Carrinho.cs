6ee5410cbc8337b185bce89dbc1bf3697c4cf4f3
037500 <037500@WKS-E-LAB2-30>
2019-10-11 21:57:34 -0300

    Adicao da classe Carrinho

---
+++ Desenvolvimento/SRC/ProjetoEcommerce/ProjetoEcommerce.Dominio/Entidades/Carrinho/Carrinho.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoEcommerce.Dominio.Entidades.Carrinho
+{
+    class Carrinho
+    {
+
+
+        //LOG
+        public int Status { get; set; }
+        public string Usuaario { get; set; }
+        public DateTime CriadoEm { get; set; }
+        public DateTime AtualizadoEm { get; set; }
+
+        //Properties
+        public int CarrinhoID { get; set; }
+        public int EnderecoId { get; set; }
+        public decimal PrecoTotal { get; set; }
+        public decimal PrecoFrete { get; set; }
+
+        public string Usuario { get; set; } // MUDAR PARA A CLASSE DEPOIS
+        public string Endereco { get; set; } // MUDAR PARA A CLASSE DEPOIS
+    }
+}
