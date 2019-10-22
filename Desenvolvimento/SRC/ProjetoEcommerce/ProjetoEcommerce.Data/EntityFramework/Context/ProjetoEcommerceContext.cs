c497ffef716ec61b29926ead254f215d1a238d11
guilhermepereira <guilhermepereira@uniaraxa.edu.br>
2019-10-22 18:56:03 -0300

    aula2210

---
+++ Desenvolvimento/SRC/ProjetoEcommerce/ProjetoEcommerce.Data/EntityFramework/Context/ProjetoEcommerceContext.cs
@@ -0,0 +1,34 @@
+﻿using ProjetoEcommerce.Data.EntityFramework.Configuration;
+using ProjetoEcommerce.Dominio.Entidades.Marketplace;
+using System.Configuration;
+using System.Data.Entity;
+
+namespace ProjetoEcommerce.Data.EntityFramework.Context
+{
+    public class ProjetoEcommerceContext : DbContext
+    {
+        public DbSet<Rastreamento> rastreamento { get; set; }
+
+        static ProjetoEcommerceContext()
+        {
+            Database.SetInitializer<ProjetoEcommerceContext>(null);
+        }
+
+        private static string ConnectionString()
+        {
+            return ConfigurationManager.ConnectionStrings["Context"].ConnectionString;
+        }
+
+        public ProjetoEcommerceContext() : base(ConnectionString())
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Configurations.Add(new RastreamentoConfiguration());
+        }
+    }
+}
---
+++ Desenvolvimento/SRC/ProjetoEcommerce/ProjetoEcommerce.Data/Repositories/RastreamentoRepository.cs
@@ -0,0 +1,19 @@
+﻿using ProjetoEcommerce.Data.Repositories.Base;
+using ProjetoEcommerce.Dominio.Entidades.Marketplace;
+using ProjetoEcommerce.Dominio.Interfaces.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoEcommerce.Data.Repositories
+{
+    public class RastreamentoRepository : RepositoryBase<Rastreamento>
+    {
+        public Rastreamento GetOne(int rastreamentoId)
+        {
+            return GetOne(rastreamentoId);
+        }
+    }
+}
