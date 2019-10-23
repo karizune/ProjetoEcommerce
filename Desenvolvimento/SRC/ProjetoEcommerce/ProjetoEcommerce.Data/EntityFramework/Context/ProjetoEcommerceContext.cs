8baeedff7f99e6334b9c3438e0186a425c35e3d4
Alefe Eliel <candidoalefe@outlook.com>
2019-10-22 21:52:04 -0300

    Aula de Configuração do Entity Framework Carrinho Compras**

---
+++ Desenvolvimento/SRC/ProjetoEcommerce/ProjetoEcommerce.Data/EntityFramework/Context/ProjetoEcommerceContext.cs
@@ -0,0 +1,41 @@
+﻿using ProjetoEcommerce.Data.EntityFramework.Configuration;
+using ProjetoEcommerce.Dominio.Entidades.Carrinho;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoEcommerce.Data.EntityFramework.Context
+{
+    class ProjetoEcommerceContext: DbContext
+    {
+
+        public DbSet<CarrinhoProduto> carrinhoProduto { get; set; }
+
+        static ProjetoEcommerceContext()
+        {
+            Database.SetInitializer<ProjetoEcommerceContext>(null);
+        }
+
+        private static string ConnectionString()
+        {
+            return ConfigurationManager.ConnectionStrings["projetoEcommerceContext"].ConnectionString;
+        }
+
+        public ProjetoEcommerceContext() : base(ConnectionString())
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Configurations.Add(new CarrinhoProdutoConfiguration());
+        }
+
+    }
+}
