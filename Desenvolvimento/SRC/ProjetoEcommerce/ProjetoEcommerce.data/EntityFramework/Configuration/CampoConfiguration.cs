e692a1cb3243e82b44209a4946f193ff5aa955cc
gabriel <gabriel.henrique@outlook.com>
2019-10-22 21:48:58 -0300

    22.10.2019

---
+++ Desenvolvimento/SRC/ProjetoEcommerce/ProjetoEcommerce.data/EntityFramework/Configuration/CampoConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using ProjetoEcommerce.Dominio.Entidades.Relatorio;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoEcommerce.data.EntityFramework.Configuration
+{
+    class CampoConfiguration : EntityTypeConfiguration<Campo>
+    {
+        public CampoConfiguration()
+        {
+            ToTable("Campo");
+            HasKey(x => x.CampoID);
+
+            Property(x => x.CampoID)
+                .HasColumnName(@"CampoID")
+                .HasColumnType("int")
+                .IsRequired()
+                .HasDatabaseGeneratedOption
+                (System.ComponentModel)
+        }
+    }
+}
---
+++ Desenvolvimento/SRC/ProjetoEcommerce/ProjetoEcommerce.data/EntityFramework/Context/ProjetoEcommerceContext.cs
@@ -0,0 +1,39 @@
+﻿using ProjetoEcommerce.Dominio.Entidades.Relatorio;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoEcommerce.data.EntityFramework.Context
+{
+    public class ProjetoEcommerceContext: DbContext
+  
+    {
+        public DbSet<Campo> campo { get; set; }
+        public DbSet<CampoRelatorio> campoRelatori { get; set; }
+        public DbSet<Relatorio> relatorio { get; set; }
+        public DbSet<TipoCampo> TipoCampo { get; set; }
+
+        static ProjetoEcommerceContext()
+        {
+            Database.SetInitializer<ProjetoEcommerceContext>(null);
+        }
+        
+        private static string ConnectionsString ()
+        {
+            return ConfigurationManager.ConnectionStrings["Context"].ConnectionString;
+        }
+        public ProjetoEcommerceContext() : base(ConnectionsString())
+        {
+
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // modelBuilder.Configurations.Add(new RastrementoConfiguration());
+        }
+    }
+}
