0fff7468546addfa0dfc97f2376a38fbdd114fed
wlborges <wellingtonborges94@hotmail.com>
2019-10-22 21:42:13 -0300

    configuration

---
+++ Desenvolvimento/SRC/ProjetoEcommerce/ProjetoEcommerce.Data/EntityFramework/Configuration/ClienteConfiguration.cs
@@ -0,0 +1,45 @@
+﻿using ProjetoEcommerce.Dominio.Entidades.Perfil;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoEcommerce.Data.EntityFramework.Configuration
+{
+    public class ClienteConfiguration: EntityTypeConfiguration<Cliente>
+    {
+        public ClienteConfiguration()
+        {
+            ToTable("Cliente");
+            HasKey(x => x.IDCLiente);
+
+            Property(x => x.IDCLiente)
+                .HasColumnName(@"IDCliente")
+                .HasColumnType("int")
+                .IsRequired()
+                .HasDatabaseGeneratedOption(System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.Identity);
+
+            Property(x => x.Nome)
+                .HasColumnName(@"Nome")
+                .HasColumnType("string")
+                .IsRequired();
+
+            Property(x => x.Senha)
+                .HasColumnName(@"Senha")
+                .HasColumnType("string")
+                .IsRequired();
+
+            Property(x => x.Tipo)
+                .HasColumnName(@"Tipo")
+                .HasColumnType("bool")
+                .IsRequired();
+
+            Property(x => x.Foto)
+                .HasColumnName(@"Foto")
+                .HasColumnType("string")
+                .IsRequired();
+        }
+    }
+}
---
+++ Desenvolvimento/SRC/ProjetoEcommerce/ProjetoEcommerce.Data/EntityFramework/Context/ProjetoEcommerceContext.cs
@@ -0,0 +1,46 @@
+﻿using ProjetoEcommerce.Data.EntityFramework.Configuration;
+using ProjetoEcommerce.Dominio.Entidades.Perfil;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoEcommerce.Data.EntityFramework.Context
+{
+    public class ProjetoEcommerceContext : DbContext
+    {
+        public DbSet<Cliente> clientes { get; set; }
+        public DbSet<Email> email { get; set; }
+        public DbSet<Fisico> fisico { get; set; }
+        public DbSet<Juridico> juridico { get; set; }
+
+        static ProjetoEcommerceContext()
+        {
+            Database.SetInitializer<ProjetoEcommerceContext>(null);
+        }
+        private static string ConnectionString()
+        {
+            return ConfigurationManager.ConnectionStrings["Context"].ConnectionString;
+        }
+
+        public ProjetoEcommerceContext() : base(ConnectionString())
+        {
+
+        }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Configurations.Add(new ClienteConfiguration());
+        }
+
+
+    }
+}
