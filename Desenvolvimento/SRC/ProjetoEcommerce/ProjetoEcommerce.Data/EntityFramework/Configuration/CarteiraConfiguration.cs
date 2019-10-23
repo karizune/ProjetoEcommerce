eb7c1a48fabe116457b9868d48b25bfb76bd7dcf
Gabriel Valeriano Gomes <gabrielvaleriano18@outlook.com>
2019-10-22 21:55:56 -0300

    CarteiraConfiguration Adicionada

--- Desenvolvimento/SRC/ProjetoEcommerce/ProjetoEcommerce.Data/EntityFramework/Configuration/CarteiraConfiguration.cs
+++ Desenvolvimento/SRC/ProjetoEcommerce/ProjetoEcommerce.Data/EntityFramework/Configuration/CarteiraConfiguration.cs
@@ -20,6 +20,34 @@ namespace ProjetoEcommerce.Data.EntityFramework.Configuration
                 .HasColumnType("int")
                 .IsRequired()
                 .HasDatabaseGeneratedOption(System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.Identity);
+
+            Property(x => x.Saldo)
+                .HasColumnName(@"Saldo")
+                .HasColumnType("decimal")
+                .IsRequired();
+
+            Property(x => x.UsuarioID)
+                .HasColumnName(@"UsuarioID")
+                .HasColumnType("int")
+                .IsRequired();
+
+            Property(x => x.AtualizadoEm)
+                .HasColumnName(@"AtualizadoEm")
+                .HasColumnType("datetime")
+                .IsRequired();
+            Property(x => x.CriadoEm)
+                .HasColumnName(@"CriadoEm")
+                .HasColumnType("datetime")
+                .IsRequired();
+            Property(x => x.Status)
+                .HasColumnName(@"Status")
+                .HasColumnType("int")
+                .IsRequired();
+            Property(x => x.Usuario)
+                .HasColumnName(@"Usuario")
+                .HasColumnType("varchar(50)")
+                .IsRequired();
+
            
         }
     }
