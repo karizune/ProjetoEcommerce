36275efbcd449a7f9f3384934cffdeb3b7e152b9
luan galan <luanchrystian2@hotmail.com>
2019-10-22 21:09:49 -0300

    the new pull request

--- Desenvolvimento/SRC/ProjetoEcommerce/ProjetoEcommerce.Data/EntityFramework/Context/ProjetoEcommerceContext.cs
+++ Desenvolvimento/SRC/ProjetoEcommerce/ProjetoEcommerce.Data/EntityFramework/Context/ProjetoEcommerceContext.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ProjetoEcommerce.Data.EntityFramework.Context
 {
-    public class ProjetoEcommerceContext : DBContext
+    public class ProjetoEcommerceContext : DbContext
     {
-
+        public DbSet<Rastreamento> rastreamento { get;  set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
