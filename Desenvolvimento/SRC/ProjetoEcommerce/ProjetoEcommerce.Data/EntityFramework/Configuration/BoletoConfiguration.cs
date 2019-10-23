fd5e1cf8c3b43e3c788794fe31abec9efb73e408
Gabriel Valeriano Gomes <gabrielvaleriano18@outlook.com>
2019-10-22 21:39:34 -0300

    parte 2 DDD

---
+++ Desenvolvimento/SRC/ProjetoEcommerce/ProjetoEcommerce.Data/EntityFramework/Configuration/BoletoConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using ProjetoEcommerce.Dominio.Entidades.Pagamento;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoEcommerce.Data.EntityFramework.Configuration
+{
+    class BoletoConfiguration : EntityTypeConfiguration<Boleto>
+    {
+        public BoletoConfiguration()
+        {
+            ToTable("Boleto");
+            HasKey(x => x.BoletoID);
+            Property(x => x.BoletoID).HasColumnName(@"BoletoID").HasColumnType("int").IsRequired().HasDatabaseGeneratedOption(System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.Identity);
+
+        }
+    }
+}
