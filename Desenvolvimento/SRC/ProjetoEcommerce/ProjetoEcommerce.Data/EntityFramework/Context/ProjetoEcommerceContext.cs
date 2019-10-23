77b21d8fa8e9686131589eb9c88dd5221ffac135
Ismael Filipe <ismaelfilipe59@hotmail.com>
2019-10-22 21:53:11 -0300

    Aula 22/10/2019

---
+++ Desenvolvimento/SRC/ProjetoEcommerce/ProjetoEcommerce.Data/EntityFramework/Context/ProjetoEcommerceContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoEcommerce.Data.EntityFramework.Context
+{
+    public class ProjetoEcommerceContext : DbContext
+    {
+        static ProjetoEcommerceContext()
+        {
+            Database.SetInitializer<ProjetoEcommerceContext>(null);
+        }
+        
+        private static string ConnectionString()
+        {
+            return ConfigurationManager.ConnectionStrings["ProjetoEcommerceContext"].ConnectionString;
+        }
+        public ProjetoEcommerceContext() : base(ConnectionString())
+        {
+
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            //modelBuilder.Configurations.Add(new RastreamentoConfiguratio());
+        }
+    }
+}
