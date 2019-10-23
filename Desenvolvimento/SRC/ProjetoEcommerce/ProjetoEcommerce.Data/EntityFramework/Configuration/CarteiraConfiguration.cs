011ad3ba696e7a86caf35307d8844160ed268949
Gabriel Valeriano Gomes <gabrielvaleriano18@outlook.com>
2019-10-22 21:29:59 -0300

    Infra DDD Parte 2

---
+++ Desenvolvimento/SRC/ProjetoEcommerce/ProjetoEcommerce.Data/EntityFramework/Configuration/CarteiraConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using ProjetoEcommerce.Dominio.Entidades.Pagamento;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoEcommerce.Data.EntityFramework.Configuration
+{
+    class CarteiraConfiguration : EntityTypeConfiguration<Carteira>
+    {
+        public CarteiraConfiguration()
+        {
+            ToTable("Carteira");
+            HasKey(x => x.CarteiraID);
+
+            Property(x => x.CarteiraID)
+                .HasColumnName(@"CarteiraID")
+                .HasColumnType("int")
+                .IsRequired()
+                .HasDatabaseGeneratedOption(System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.Identity);
+           
+        }
+    }
+}
--- Desenvolvimento/SRC/ProjetoEcommerce/ProjetoEcommerce.Data/EntityFramework/Context/ProjetoEcommerceContext.cs
+++ Desenvolvimento/SRC/ProjetoEcommerce/ProjetoEcommerce.Data/EntityFramework/Context/ProjetoEcommerceContext.cs
@@ -1,13 +1,43 @@
-﻿using System;
+﻿using ProjetoEcommerce.Dominio.Entidades.Pagamento;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ProjetoEcommerce.Data.EntityFramework.Context
 {
-    public class ProjetoEcommerceContext : DBContext
+    public class ProjetoEcommerceContext : DbContext
     {
+        public DbSet<Boleto> Boleto { get; set; }
+        public DbSet<BandeiraCartao> BandeiraCartao { get; set; }
+        public DbSet<Cartao> Cartao { get; set; }
+        public DbSet<Cartao_Tipo> Cartao_Tipo { get; set; }
+        public DbSet<Carteira> Carteira { get; set; }
+        public DbSet<MetodoPagamento> MetodoPagamentos { get; set; }
+        public DbSet<Pagamento> Pagamento { get; set; }
+        public DbSet<StatusCartao> StatusCartao { get; set; }
+        public DbSet<StatusPagamento> StatusPagamento { get; set; }
+        public DbSet<TipoCartao> TipoCartao { get; set; }
 
+        static ProjetoEcommerceContext()
+        {
+            Database.SetInitializer<ProjetoEcommerceContext>(null);
+        }
+        private static string ConnectionString()
+        {
+            return ConfigurationManager.ConnectionStrings["Context"].ConnectionString;
+        }
+        public ProjetoEcommerceContext():base(ConnectionString())
+        {
+
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        }
     }
 }
