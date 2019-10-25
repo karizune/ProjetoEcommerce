6daf3a5333b800ed0b136f2d87ebb0fe32aeae1a
gabriel <gabriel.henrique@outlook.com>
2019-10-25 20:39:19 -0300

    Gabriel1

--- Desenvolvimento/SRC/ProjetoEcommerce/ProjetoEcommerce.Dominio/Entidades/Relatorio/Relatorio.cs
+++ Desenvolvimento/SRC/ProjetoEcommerce/ProjetoEcommerce.Dominio/Entidades/Relatorio/Relatorio.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace ProjetoEcommerce.Dominio.Entidades.Relatorio
 {
-    class Relatorio : IEntidadeBase
+    public class Relatorio : IEntidadeBase
     {
         public int RelatorioID { get; set; }
         public string Nome { get; set; }
---
+++ Desenvolvimento/SRC/ProjetoEcommerce/ProjetoEcommerce.data/EntityFramework/Configuration/RelatorioConfiguration.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace ProjetoEcommerce.data.EntityFramework.Configuration
+{
+    class RelatorioConfiguration : EntityTypeConfiguration<Relatorio>
+    {
+        public CampoConfiguration()
+        {
+            ToTable("Relatorio");
+            HasKey(x => x.RelatorioID);
+
+            Property(x => x.CampoID)
+                .HasColumnName(@"RelatorioID")
+                .HasColumnType("int")
+                .IsRequired()
+                .HasDatabaseGeneratedOption
+                (System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.Identity);
+
+            Property(x => x.Usuario)
+                .HasColumnName(@"Usuario")
+                .HasColumnType("int")
+                .IsRequired()
+                .HasDatabaseGeneratedOption
+                (System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.Identity);
+
+            Property(x => x.Usuario)
+                .HasColumnName(@"Nome")
+                .HasColumnType("varchar")
+                .IsRequired()
+                .HasDatabaseGeneratedOption
+                (System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.Identity);
+
+            Property(x => x.Status)
+                .HasColumnName(@"Descricao")
+                .HasColumnType("varchar")
+                .IsRequired()
+                .HasDatabaseGeneratedOption
+                (System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.Identity);
+
+            Property(x => x.CriadoEm)
+                .HasColumnName(@"Status")
+                .HasColumnType("int")
+                .IsRequired()
+                .HasDatabaseGeneratedOption
+                (System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.Identity);
+
+            Property(x => x.AtualizadoEm)
+                .HasColumnName(@"CriadoEm")
+                .HasColumnType("datetime")
+                .IsRequired()
+                .HasDatabaseGeneratedOption
+                (System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.Identity);
+
+            Property(x => x.TipoCampoID)
+                .HasColumnName(@"AtualizadoEm")
+                .HasColumnType("int")
+                .IsRequired()
+                .HasDatabaseGeneratedOption
+                (System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.Identity);
+        }
+    }
+}
