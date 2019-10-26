62745788d7ae19f2a90b94e62a0470d329328065
Marciel Felipe <42660706+marcielfelipe@users.noreply.github.com>
2019-10-25 21:43:53 -0300

    correção configuratio

--- Desenvolvimento/SRC/ProjetoEcommerce/ProjetoEcommerce.Data/Configuration/CategoriaConfiguration.cs
+++ Desenvolvimento/SRC/ProjetoEcommerce/ProjetoEcommerce.Data/Configuration/CategoriaConfiguration.cs
@@ -13,30 +13,24 @@ namespace ProjetoEcommerce.Data.Configuration
         public CategoriaConfiguration()
         {
             //CategoriaID
-            ToTable("Categoria");
+            ToTable("Categoria","produto");
             HasKey(x => x.CategoriaID);
             Property(x => x.CategoriaID).HasColumnName(@"CategogiaID").HasColumnType("int").IsRequired().
             HasDatabaseGeneratedOption(System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.Identity);
             //Nome
-            ToTable("Categoria");
-            HasKey(x => x.Nome);
+
             Property(x => x.Nome).HasColumnName(@"Nome").HasColumnType("varchar").IsRequired();
 
             //Status
-            ToTable("Categoria");
-            HasKey(x => x.Status);
+
             Property(x => x.Status).HasColumnName(@"Status").HasColumnType("int").IsRequired();
             //Usuario
-            ToTable("Categoria");
-            HasKey(x => x.Usuario);
+
             Property(x => x.Usuario).HasColumnName(@"Usuario").HasColumnType("varchar").IsRequired();
             //CriadoEm
-            ToTable("Categoria");
-            HasKey(x => x.AtualizadoEm);
+
             Property(x => x.AtualizadoEm).HasColumnName(@"CriadoEm").HasColumnType("datetime").IsRequired();
             //AtualizadoEm
-            ToTable("Categoria");
-            HasKey(x => x.AtualizadoEm);
             Property(x => x.AtualizadoEm).HasColumnName(@"AtualizadoEm").HasColumnType("datetime").IsRequired();
         }
     }
