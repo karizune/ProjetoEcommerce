f1c079376e912c0131051e4989e4bf5cc1013cfc
visualstudio <visualstudio@WKS-E-LAB2-38>
2019-10-25 21:36:16 -0300

    Criação Configuration Marca e Produto

--- Desenvolvimento/SRC/ProjetoEcommerce/ProjetoEcommerce.Data/Configuration/MarcaConfiguration.cs
+++ Desenvolvimento/SRC/ProjetoEcommerce/ProjetoEcommerce.Data/Configuration/MarcaConfiguration.cs
@@ -1,12 +1,43 @@
-﻿using System;
+﻿using ProjetoEcommerce.Dominio.Entidades.Produto;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ProjetoEcommerce.Data.Configuration
 {
-    class MarcaConfiguration
+    public class MarcaConfiguration : EntityTypeConfiguration<Marca>
     {
+        public MarcaConfiguration()
+        {
+            //MarcaID
+            ToTable("Marca");
+            HasKey(x => x.MarcaID);
+            Property(x => x.MarcaID).HasColumnName(@"MarcaID").HasColumnType("int").IsRequired().
+            HasDatabaseGeneratedOption(System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.Identity);
+            //Nome
+            ToTable("Marca");
+            HasKey(x => x.Nome);
+            Property(x => x.Nome).HasColumnName(@"Nome").HasColumnType("varchar").IsRequired();
+
+            //Status
+            ToTable("Marca");
+            HasKey(x => x.Status);
+            Property(x => x.Status).HasColumnName(@"Status").HasColumnType("int").IsRequired();
+            //Usuario
+            ToTable("Marca");
+            HasKey(x => x.Usuario);
+            Property(x => x.Usuario).HasColumnName(@"Usuario").HasColumnType("varchar").IsRequired();
+            //CriadoEm
+            ToTable("Marca");
+            HasKey(x => x.CriadoEm);
+            Property(x => x.CriadoEm).HasColumnName(@"CriadoEm").HasColumnType("datetime").IsRequired();
+            //AtualizadoEm
+            ToTable("Marca");
+            HasKey(x => x.AtualizadoEm);
+            Property(x => x.AtualizadoEm).HasColumnName(@"AtualizadoEm").HasColumnType("datetime").IsRequired();
+        }
     }
 }
