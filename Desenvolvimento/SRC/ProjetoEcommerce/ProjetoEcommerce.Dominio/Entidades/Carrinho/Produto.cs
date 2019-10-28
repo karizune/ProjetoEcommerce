d81a48c65588cdfbc114977dae51e570d05c502d
vitorbarreto <vitorbarreto@uniaraxa.edu.br>
2019-10-28 21:51:27 -0200

    Exemplo controller

---
+++ Desenvolvimento/SRC/ProjetoEcommerce/ProjetoEcommerce.Dominio/Entidades/Carrinho/Produto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoEcommerce.Dominio.Entidades.Carrinho
+{
+    public class Produto
+    {
+        public string Nome { get; set; }
+        public decimal Qtd { get; set; }
+        public decimal Preco { get; set; }
+    }
+}
--- Desenvolvimento/SRC/ProjetoEcommerce/ProjetoEcommerce/Controllers/CarrinhoController.cs
+++ Desenvolvimento/SRC/ProjetoEcommerce/ProjetoEcommerce/Controllers/CarrinhoController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProjetoEcommerce.Dominio.Entidades.Carrinho;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +12,32 @@ namespace ProjetoEcommerce.Controllers
         // GET: Carrinho
         public ActionResult Index()
         {
-            return View();
+
+            List<Produto> lista = new List<Produto>();
+
+            Produto p1 = new Produto {
+                Nome = "Livro",
+                Qtd = 9,
+                Preco = 270
+            };
+            Produto p2 = new Produto {
+                Nome = "DM Screen do Germano",
+                Qtd = 1,
+                Preco = 200
+            };
+            Produto p3 = new Produto {
+                Nome = "Sabao",
+                Qtd = 2,
+                Preco = 10
+            };
+
+            lista.Add(p1);
+            lista.Add(p2);
+            lista.Add(p3);
+
+
+
+            return View("Index",lista);
         }
     }
 }
