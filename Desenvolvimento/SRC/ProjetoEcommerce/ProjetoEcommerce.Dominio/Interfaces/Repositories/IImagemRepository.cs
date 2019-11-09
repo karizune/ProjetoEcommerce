7c6477e6eba436ac707d0ae93401655b524bf9a2
visualstudio <visualstudio@WKS-E-LAB2-38>
2019-11-08 21:08:31 -0300

    Criação iinterface repositório

---
+++ Desenvolvimento/SRC/ProjetoEcommerce/ProjetoEcommerce.Dominio/Interfaces/Repositories/IImagemRepository.cs
@@ -0,0 +1,14 @@
+﻿using ProjetoEcommerce.Dominio.Entidades.Produto;
+using ProjetoEcommerce.Dominio.Interfaces.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoEcommerce.Dominio.Interfaces.Repositories
+{
+    interface IImagemRepository
+    {
+    }
+}
