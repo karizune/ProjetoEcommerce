f5b1bdb16753f00517453ab2c55566019397f9c9
Matheus Lourenço <matheus98lourenco@gmail.com>
2019-11-12 21:54:27 -0300

    View de criação de cartão

--- Desenvolvimento/SRC/ProjetoEcommerce/ProjetoEcommerce.Dominio/Entidades/Pagamento/Cartao.cs
+++ Desenvolvimento/SRC/ProjetoEcommerce/ProjetoEcommerce.Dominio/Entidades/Pagamento/Cartao.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProjetoEcommerce.Dominio.Entidades.Pagamento
 {
     public class Cartao : BaseEntity
     {
         public int CartaoID { get; set; }
+        [CreditCard (ErrorMessage = "Insira um cartão válido")]
         public string Numero { get; set; }
         public DateTime Vencimento { get; set; }
         public int UsuarioID { get; set; }
---
+++ Desenvolvimento/SRC/ProjetoEcommerce/ProjetoEcommerce/Controllers/Pagamento/CartaoController.cs
@@ -0,0 +1,33 @@
+﻿using ProjetoEcommerce.Data.EntityFramework.Context;
+using ProjetoEcommerce.Dominio.Entidades.Pagamento;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjetoEcommerce.Controllers.Pagamento
+{
+    public class CartaoController : Controller
+    {
+        ProjetoEcommerceContext context = new ProjetoEcommerceContext();
+        public ActionResult Index()
+        {
+        
+            return View();
+        }
+        public ActionResult Criar()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Criar(Cartao cartao)
+        {
+            context.Cartao.Add(cartao);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
