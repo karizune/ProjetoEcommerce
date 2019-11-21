ab6bc900302be36d5b476c6616aa8887fbaaa62b
Wayter Paulo <waytermascara@hotmail.com>
2019-11-20 21:51:30 -0300

    Configuração AutoMapper

--- Desenvolvimento/SRC/ProjetoEcommerce/ProjetoEcommerce/AutoMapper/DomainToViewModelMappingProfile.cs
+++ Desenvolvimento/SRC/ProjetoEcommerce/ProjetoEcommerce/AutoMapper/DomainToViewModelMappingProfile.cs
@@ -1,11 +1,28 @@
-﻿using System;
+﻿using AutoMapper;
+using ProjetoEcommerce.Dominio.Entidades.Perfil;
+using ProjetoEcommerce.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace ProjetoEcommerce.AutoMapper
 {
-    public class DomainToViewModelMappingProfile
+    public class DomainToViewModelMappingProfile : Profile
     {
+        public override string ProfileName
+        {
+            get { return "ViewModelToDomainMappings"; }
+        }
+
+        protected override void Configure()
+        {
+            Mapper.CreateMap<ClienteViewModel, Cliente>();
+            Mapper.CreateMap<JuridicoViewModel, Juridico>();
+            Mapper.CreateMap<FisicoViewModel, Fisico>();
+            Mapper.CreateMap<TelefoneViewModel, Telefone>();
+            Mapper.CreateMap<EmailViewModel, Email>();
+            Mapper.CreateMap<SexoViewModel, Sexo>();
+        }
     }
 }
--- Desenvolvimento/SRC/ProjetoEcommerce/ProjetoEcommerce/AutoMapper/ViewModelToDomainMappingProfile.cs
+++ Desenvolvimento/SRC/ProjetoEcommerce/ProjetoEcommerce/AutoMapper/ViewModelToDomainMappingProfile.cs
@@ -1,11 +1,28 @@
-﻿using System;
+﻿using AutoMapper;
+using ProjetoEcommerce.Dominio.Entidades.Perfil;
+using ProjetoEcommerce.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace ProjetoEcommerce.AutoMapper
 {
-    public class ViewModelToDomainMappingProfile
+    public class ViewModelToDomainMappingProfile : Profile
     {
+        public override string ProfileName
+        {
+            get { return "DomainToViewModelMappings"; }
+        }
+
+        protected override void Configure()
+        {
+            Mapper.CreateMap<Cliente, ClienteViewModel >();
+            Mapper.CreateMap<Juridico, JuridicoViewModel>();
+            Mapper.CreateMap<Fisico, FisicoViewModel>();
+            Mapper.CreateMap<Telefone, TelefoneViewModel>();
+            Mapper.CreateMap<Email, EmailViewModel>();
+            Mapper.CreateMap<Sexo, SexoViewModel>();
+        }
     }
 }
