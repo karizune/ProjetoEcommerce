e084af40dbaab05f9d44187fe8bf3422ee7382f4
Marciel Felipe <42660706+marcielfelipe@users.noreply.github.com>
2019-11-22 20:54:57 -0300

    Delete namespace

--- Desenvolvimento/SRC/ProjetoEcommerce/ProjetoEcommerce.Dominio/Entidades/Produto/Marca.cs
+++ Desenvolvimento/SRC/ProjetoEcommerce/ProjetoEcommerce.Dominio/Entidades/Produto/Marca.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ProjetoEcommerce.Dominio.Entidades.Produto
+namespace ProjetoEcommerce.Dominio.Entidades
 {
     public class Marca:IEntityBase
     {
