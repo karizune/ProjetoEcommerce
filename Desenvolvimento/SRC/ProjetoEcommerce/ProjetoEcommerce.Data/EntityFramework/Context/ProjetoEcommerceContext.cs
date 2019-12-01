199cde569f0d4db6365ac78a7002ec76620fe016
Marciel Felipe <42660706+marcielfelipe@users.noreply.github.com>
2019-12-01 15:08:20 -0300

    Context

---
+++ Desenvolvimento/SRC/ProjetoEcommerce/ProjetoEcommerce.Data/EntityFramework/Context/ProjetoEcommerceContext.cs
@@ -0,0 +1,43 @@
+﻿using ProjetoEcommerce.Data.Configuration;
+using ProjetoEcommerce.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoEcommerce.Data.EntityFramework.Context
+{
+    public class ProjetoEcommerceContext : DbContext
+    {
+        public DbSet<Categoria> Categoria { get; set; }
+        public DbSet<Imagem> Imagem { get; set; }
+        public DbSet<Loja> Loja { get; set; }
+        public DbSet<Marca> Marca { get; set; }
+        public DbSet<Produto> Produto { get; set; }
+
+        static ProjetoEcommerceContext()
+        {
+            Database.SetInitializer<ProjetoEcommerceContext>(null);
+        }
+        public static string ConnectionString()
+        {
+            return ConfigurationManager.ConnectionStrings["projetoEcommerceContext"].ConnectionString;
+        }
+        public ProjetoEcommerceContext() : base(ConnectionString())
+        {
+
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Configurations.Add(new CategoriaConfiguration());
+            modelBuilder.Configurations.Add(new ImagemConfiguration());
+            modelBuilder.Configurations.Add(new LojaConfiguration());
+            modelBuilder.Configurations.Add(new MarcaConfiguration());
+            modelBuilder.Configurations.Add(new ProdutoConfiguration());
+        }
+    }
+}
