dfe81c1b4d1197654ac9c4c155882f79eb8b21aa
Marciel Felipe <42660706+marcielfelipe@users.noreply.github.com>
2019-12-01 15:12:06 -0300

    ProdutoRepository

---
+++ Desenvolvimento/SRC/ProjetoEcommerce/ProjetoEcommerce.Data/Repositories/Produto/CatogoriaRepository.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.Entity;
+using System.Collections.Generic;
+using System.Linq;
+using ProjetoEcommerce.Dominio.Entidades;
+using ProjetoEcommerce.Dominio.Interfaces.Repositories;
+using ProjetoEcommerce.Data.EntityFramework;
+
+namespace ProjetoEcommerce.Data.Repositories.Base
+{
+    public class CategoriaRepository : RepositoryBase<Categoria>, ICategoriaRepository
+    {
+        public IEnumerable<Categoria> BuscarAtivos()
+        {
+            return Entity.Where(f => f.Status == 1);
+        }
+
+        public Categoria GetOneBy(int CategoriaID)
+        {
+            return GetOne(CategoriaID);
+        }
+    }
+}
