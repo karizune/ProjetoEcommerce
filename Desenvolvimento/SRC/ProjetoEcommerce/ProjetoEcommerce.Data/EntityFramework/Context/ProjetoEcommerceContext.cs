1ff54fce91c6e0b8129d63edb06fecd82e899586
Marciel Felipe <42660706+marcielfelipe@users.noreply.github.com>
2019-12-03 20:29:38 -0300

    CONTEXT

--- Desenvolvimento/SRC/ProjetoEcommerce/ProjetoEcommerce.Data/EntityFramework/Context/ProjetoEcommerceContext.cs
+++ Desenvolvimento/SRC/ProjetoEcommerce/ProjetoEcommerce.Data/EntityFramework/Context/ProjetoEcommerceContext.cs
@@ -1,17 +1,18 @@
 ﻿using ProjetoEcommerce.Data.Configuration;
+using ProjetoEcommerce.Data.EntityFramework.Configuration;
 using ProjetoEcommerce.Dominio.Entidades;
-using System;
-using System.Collections.Generic;
+using ProjetoEcommerce.Dominio.Entidades.Marketplace;
+using ProjetoEcommerce.Dominio.Entidades.Pagamento;
 using System.Configuration;
 using System.Data.Entity;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ProjetoEcommerce.Data.EntityFramework.Context
 {
     public class ProjetoEcommerceContext : DbContext
     {
+        public DbSet<Rastreamento> rastreamento { get; set; }
+        public DbSet<TipoCartao> TipoCartao { get; set; }
+        //PRODUTO
         public DbSet<Categoria> Categoria { get; set; }
         public DbSet<Imagem> Imagem { get; set; }
         public DbSet<Loja> Loja { get; set; }
@@ -22,17 +23,24 @@ namespace ProjetoEcommerce.Data.EntityFramework.Context
         {
             Database.SetInitializer<ProjetoEcommerceContext>(null);
         }
-        public static string ConnectionString()
+
+        private static string ConnectionString()
         {
             return ConfigurationManager.ConnectionStrings["projetoEcommerceContext"].ConnectionString;
         }
+
         public ProjetoEcommerceContext() : base(ConnectionString())
         {
 
         }
+
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Configurations.Add(new RastreamentoConfiguration());
+            modelBuilder.Configurations.Add(new TipoCartaoConfiguration());
+            //PRODUTO
             modelBuilder.Configurations.Add(new CategoriaConfiguration());
             modelBuilder.Configurations.Add(new ImagemConfiguration());
             modelBuilder.Configurations.Add(new LojaConfiguration());
