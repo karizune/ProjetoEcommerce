53ad5364d434df8ceaf0ec1f2595f4197f98d175
Visual Studio <visualstudio@campus1.uniaraxa.edu.br>
2019-12-03 21:08:59 -0300

    Repositorios de acesso

---
+++ Desenvolvimento/SRC/ProjetoEcommerce/ProjetoEcommerce.Dominio/Interfaces/Repositories/IFormularioGrupoRepository.cs
@@ -0,0 +1,14 @@
+﻿using ProjetoEcommerce.Dominio.Entidades.Seguranca;
+using ProjetoEcommerce.Dominio.Interfaces.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoEcommerce.Dominio.Interfaces.Repositories
+{
+    public interface IFormularioGrupoRepository : IRepositoryBase<FormularioGrupo>
+    {
+    }
+}
