d937d67247a570346cc7c46c5d0d702844dd7cd9
Walber Fellipe <Walberfellipe97@outlook.com>
2019-12-06 21:05:05 -0300

    vaiii

--- Desenvolvimento/SRC/ProjetoEcommerce/ProjetoEcommerce.Data/EntityFramework/Context/ProjetoEcommerceContext.cs
+++ Desenvolvimento/SRC/ProjetoEcommerce/ProjetoEcommerce.Data/EntityFramework/Context/ProjetoEcommerceContext.cs
@@ -1,6 +1,5 @@
 ﻿using ProjetoEcommerce.Data.EntityFramework.Configuration;
-using ProjetoEcommerce.Dominio.Entidades.Marketplace;
-using ProjetoEcommerce.Dominio.Entidades.Pagamento;
+using ProjetoEcommerce.Dominio.Entidades.Relatorio;
 using System.Configuration;
 using System.Data.Entity;
 
@@ -8,30 +7,36 @@ namespace ProjetoEcommerce.Data.EntityFramework.Context
 {
     public class ProjetoEcommerceContext : DbContext
     {
-        public DbSet<Rastreamento> rastreamento { get; set; }
-        public DbSet<TipoCartao> TipoCartao { get; set; }
+        public DbSet<Campo> Campo { get; set; }
+        public DbSet<CampoRelatorio> CampoRelatorio { get; set; }
+        public DbSet<Registro> Registro { get; set; }
+        public DbSet<TipoCampo> TipoCampo { get; set; }
+        public DbSet<Relatorio> Relatorio { get; set; }
+        public DbSet<Usuario> Usuario { get; set; }
 
         static ProjetoEcommerceContext()
         {
             Database.SetInitializer<ProjetoEcommerceContext>(null);
         }
 
-        private static string ConnectionString()
+        private static string ConnectionsString()
         {
-            return ConfigurationManager.ConnectionStrings["projetoEcommerceContext"].ConnectionString;
+            return ConfigurationManager.ConnectionStrings["ProjetoEcommerceContext"].ConnectionString;
         }
-
-        public ProjetoEcommerceContext() : base(ConnectionString())
+        public ProjetoEcommerceContext() : base(ConnectionsString())
         {
 
         }
-
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
-            modelBuilder.Configurations.Add(new RastreamentoConfiguration());
-            modelBuilder.Configurations.Add(new TipoCartaoConfiguration());
+            modelBuilder.Configurations.Add(new CampoConfiguration());
+            modelBuilder.Configurations.Add(new CampoRelatorioConfiguration());
+            modelBuilder.Configurations.Add(new RegistroConfiguration());
+            modelBuilder.Configurations.Add(new TipoCampoConfiguration());
+            modelBuilder.Configurations.Add(new RelatorioConfiguration());
+            modelBuilder.Configurations.Add(new UsuarioConfiguration());
         }
     }
 }
