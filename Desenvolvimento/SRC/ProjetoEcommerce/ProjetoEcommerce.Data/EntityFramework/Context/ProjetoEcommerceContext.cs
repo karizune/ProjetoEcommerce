c42ae37bf5726a348ee4a54d6699fff525b861e8
Carlos Junior <carlos-junior08@hotmail.com>
2019-12-13 20:53:24 -0300

    a

--- Desenvolvimento/SRC/ProjetoEcommerce/ProjetoEcommerce.Data/EntityFramework/Context/ProjetoEcommerceContext.cs
+++ Desenvolvimento/SRC/ProjetoEcommerce/ProjetoEcommerce.Data/EntityFramework/Context/ProjetoEcommerceContext.cs
@@ -1,5 +1,4 @@
-﻿using ProjetoEcommerce.data.EntityFramework.Configuration;
-using ProjetoEcommerce.Data.EntityFramework.Configuration;
+﻿using ProjetoEcommerce.Data.EntityFramework.Configuration;
 using ProjetoEcommerce.Dominio.Entidades.Pagamento;
 using ProjetoEcommerce.Dominio.Entidades.Relatorio;
 using System.Configuration;
@@ -16,14 +15,6 @@ namespace ProjetoEcommerce.Data.EntityFramework.Context
         public DbSet<Relatorio> Relatorio { get; set; }
         public DbSet<Usuario> Usuario { get; set; }
         public DbSet<TipoCartao> TipoCartao { get; set; }
-
-
-        public DbSet<Campo> Campo { get; set; }
-        public DbSet<CampoRelatorio> CampoRelatorio { get; set; }
-        public DbSet<Registro> Registro { get; set; }
-        public DbSet<Relatorio> Relatorio { get; set; }
-        public DbSet<TipoCampo> TipoCampo { get; set; }
-        public DbSet<Usuario> Usuario { get; set; }
 
         static ProjetoEcommerceContext()
         {
@@ -49,12 +40,6 @@ namespace ProjetoEcommerce.Data.EntityFramework.Context
             modelBuilder.Configurations.Add(new RelatorioConfiguration());
             modelBuilder.Configurations.Add(new UsuarioConfiguration());
             modelBuilder.Configurations.Add(new TipoCartaoConfiguration());
-
-            modelBuilder.Configurations.Add(new CampoConfiguration());
-            modelBuilder.Configurations.Add(new CampoRelatorioConfiguration());
-            modelBuilder.Configurations.Add(new RegistroConfiguration());
-            modelBuilder.Configurations.Add(new TipoCampoConfiguration());
-            modelBuilder.Configurations.Add(new UsuarioConfiguration());
 
         }
     }
