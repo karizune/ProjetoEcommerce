a111c6c013cdb7f2ed8b3cd302313b6474cd24d7
Carlos Junior <carlos-junior08@hotmail.com>
2019-12-13 20:53:19 -0300

    merge

--- Desenvolvimento/SRC/ProjetoEcommerce/ProjetoEcommerce.data/EntityFramework/Context/ProjetoEcommerceContext.cs
+++ Desenvolvimento/SRC/ProjetoEcommerce/ProjetoEcommerce.data/EntityFramework/Context/ProjetoEcommerceContext.cs
@@ -1,6 +1,4 @@
-﻿using ProjetoEcommerce.data.EntityFramework.Configuration;
-using ProjetoEcommerce.Data.EntityFramework.Configuration;
-using ProjetoEcommerce.Dominio.Entidades.Marketplace;
+﻿using ProjetoEcommerce.Data.EntityFramework.Configuration;
 using ProjetoEcommerce.Dominio.Entidades.Pagamento;
 using ProjetoEcommerce.Dominio.Entidades.Relatorio;
 using System.Configuration;
@@ -10,15 +8,11 @@ namespace ProjetoEcommerce.Data.EntityFramework.Context
 {
     public class ProjetoEcommerceContext : DbContext
     {
-        public DbSet<Rastreamento> rastreamento { get; set; }
-        public DbSet<TipoCartao> TipoCartao { get; set; }
-
-
         public DbSet<Campo> Campo { get; set; }
         public DbSet<CampoRelatorio> CampoRelatorio { get; set; }
         public DbSet<Registro> Registro { get; set; }
-        public DbSet<Relatorio> Relatorio { get; set; }
         public DbSet<TipoCampo> TipoCampo { get; set; }
+        public DbSet<Relatorio> Relatorio { get; set; }
         public DbSet<Usuario> Usuario { get; set; }
         public DbSet<TipoCartao> TipoCartao { get; set; }
 
@@ -27,29 +21,26 @@ namespace ProjetoEcommerce.Data.EntityFramework.Context
             Database.SetInitializer<ProjetoEcommerceContext>(null);
         }
 
-        private static string ConnectionString()
+        private static string ConnectionsString()
         {
-            return ConfigurationManager.ConnectionStrings["projetoEcommerceContext"].ConnectionString;
+            return ConfigurationManager.ConnectionStrings["ProjetoEcommerceContext"].ConnectionString;
         }
-
-        public ProjetoEcommerceContext() : base(ConnectionString())
+        public ProjetoEcommerceContext() : base(ConnectionsString())
         {
 
         }
-
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-
-            modelBuilder.Configurations.Add(new RastreamentoConfiguration());
-            modelBuilder.Configurations.Add(new TipoCartaoConfiguration());
 
             modelBuilder.Configurations.Add(new CampoConfiguration());
             modelBuilder.Configurations.Add(new CampoRelatorioConfiguration());
             modelBuilder.Configurations.Add(new RegistroConfiguration());
             modelBuilder.Configurations.Add(new TipoCampoConfiguration());
+            modelBuilder.Configurations.Add(new RelatorioConfiguration());
             modelBuilder.Configurations.Add(new UsuarioConfiguration());
             modelBuilder.Configurations.Add(new TipoCartaoConfiguration());
+
         }
     }
 }
